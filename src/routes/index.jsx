fc1775ca4c83406c2113e50e1c16abaf8c540621
unknown <sahusattwik@gmail.com>
2022-10-02 21:25:09 +0530

    Added instructions on start screen

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -30,7 +30,23 @@ const Home = () => {
   };
 
   return (
-    <div className="Home">
+    <div className="Home grid gap-4 h-full my-24 place-items-center">
+      <ul style={{ listStyleType: "initial" }}>
+        <li>Sign in with Google to see "pweets"</li>
+        <li>Click "+" to post your own pweet</li>
+        <li>Click on your profile picture on top right to see your profile</li>
+        <li>Click "Sign out" to sign out of your account</li>
+        <li>
+          To install PWA:
+          <ul style={{ listStyleType: "initial", paddingLeft: "2rem" }}>
+            <li>Click on three dots on top right in browser</li>
+            <li>Click "Add to Home Screen"</li>
+            <li>
+              Come back to you home screen to find the app installed and open it
+            </li>
+          </ul>
+        </li>
+      </ul>
       <button
         onClick={signinWithGooogle}
         className="px-3 py-2 bg-blue-500 text-white rounded transition-all duration-150 hover:bg-blue-600"
